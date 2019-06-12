153d3983b3c7e380885059aca72f1c94161183a7
Aditya Kulkarni <zcash@adityapk.com>
2019-06-11 20:53:49 -0700

    Call from C++

---
+++ qtlib/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+
+#include "zecpaperrust.h"
+
+using namespace std;
+
+int main() {
+  char * from_rust = rust_generate_wallet(true, 1);
+  auto stri = string(from_rust);
+  cout << stri << endl;
+  rust_free_string(from_rust);
+
+  return 0;
+}
