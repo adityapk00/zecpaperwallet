37703f42c2dfabcd091bbcd888690a2d440fe21d
Aditya Kulkarni <zcash@adityapk.com>
2019-06-19 21:44:10 -0700

    combined build

--- ui/mkrelease.sh
+++ ui/mkrelease.sh
@@ -37,6 +37,7 @@ QT_STATIC=$QT_PATH/clang_64/bin
 
 # Clean
 echo -n "Cleaning..............."
+$QT_STATIC/qmake papersapling.pro CONFIG+=release >/dev/null
 make distclean >/dev/null 2>&1
 rm -rf    artifacts/macOS-zecpaperwallet-v$APP_VERSION
 mkdir -p  artifacts/macOS-zecpaperwallet-v$APP_VERSION
@@ -60,5 +61,69 @@ cp -r zecpaperwalletui.app artifacts/macOS-zecpaperwallet-v$APP_VERSION/
 echo "[OK]"
 
 # Run inside docker container
-docker run --rm -v ${PWD}/..:/opt/zecpaperwallet zecwallet/compileenv:v0.7 bash -c "cd /opt/zecpaperwallet/ui && ./mkdockerwinlinux.sh -v $APP_VERSION"
+docker run --rm -v ${PWD}/..:/opt/zecpaperwallet zecwallet/compileenv:v0.8 bash -c "cd /opt/zecpaperwallet/ui && ./mkdockerwinlinux.sh -v $APP_VERSION"
+
+
+# Move to build the cli
+cd ../cli
+
+# Clean everything first
+cargo clean
+
+# Compile for mac directly
+cargo build --release 
+
+# For Windows and Linux, build via docker
+docker run --rm -v $(pwd)/..:/opt/zecpaperwallet rust/zecpaperwallet:v0.2 bash -c "cd /opt/zecpaperwallet/cli && cargo build --release --artifacts x86_64-unknown-linux-musl && cargo build --release --artifacts x86_64-pc-windows-gnu && cargo build --release --artifacts aarch64-unknown-linux-gnu"
+
+
+# Come back and package everything
+cd ../ui
+
+# Now sign and zip the binaries
+#macOS
+cp ../cli/target/release/zecpaperwallet artifacts/macOS-zecpaperwallet-v$APP_VERSION/
+gpg --batch --output artifacts/macOS-zecpaperwallet-v$APP_VERSION/zecpaperwallet.sig --detach-sig artifacts/macOS-zecpaperwallet-v$APP_VERSION/zecpaperwallet 
+gpg --batch --output artifacts/macOS-zecpaperwallet-v$APP_VERSION/zecpaperwallet.sig --detach-sig artifacts/macOS-zecpaperwallet-v$APP_VERSION/zecpaperwallet.app 
+cd artifacts
+cd macOS-zecpaperwallet-v$APP_VERSION
+gsha256sum zecpaperwallet zecpaperwalletui.app > sha256sum.txt
+cd ..
+zip -r macOS-zecpaperwallet-v$APP_VERSION.zip macOS-zecpaperwallet-v$APP_VERSION 
+cd ..
+
+
+#Linux
+cp ../cli/target/x86_64-unknown-linux-musl/release/zecpaperwallet artifacts/linux-zecpaperwallet-v$APP_VERSION/
+gpg --batch --output artifacts/linux-zecpaperwallet-v$APP_VERSION/zecpaperwallet.sig --detach-sig artifacts/linux-zecpaperwallet-v$APP_VERSION/zecpaperwallet
+cd artifacts
+cd linux-zecpaperwallet-v$APP_VERSION
+gsha256sum zecpaperwallet zecpaperwalletui > sha256sum.txt
+cd ..
+zip -r linux-zecpaperwallet-v$APP_VERSION.zip linux-zecpaperwallet-v$APP_VERSION 
+cd ..
+
+
+#Windows
+cp ../cli/target/x86_64-pc-windows-gnu/release/zecpaperwallet.exe artifacts/Windows-zecpaperwallet-v$APP_VERSION/
+gpg --batch --output artifacts/Windows-zecpaperwallet-v$APP_VERSION/zecpaperwallet.sig --detach-sig artifacts/Windows-zecpaperwallet-v$APP_VERSION/zecpaperwallet.exe
+cd artifacts
+cd Windows-zecpaperwallet-v$APP_VERSION
+gsha256sum zecpaperwallet.exe > sha256sum.txt
+cd ..
+zip -r Windows-zecpaperwallet-v$APP_VERSION.zip Windows-zecpaperwallet-v$APP_VERSION 
+cd ..
+
+
+# aarch64 (armv8)
+rm -rf artifacts/aarch64-zecpaperwallet-v$APP_VERSION
+mkdir -p artifacts/aarch64-zecpaperwallet-v$APP_VERSION
+cp ../cli/target/aarch64-unknown-linux-gnu/release/zecpaperwallet artifacts/aarch64-zecpaperwallet-v$APP_VERSION/
+gpg --batch --output artifacts/aarch64-zecpaperwallet-v$APP_VERSION/zecpaperwallet.sig --detach-sig artifacts/aarch64-zecpaperwallet-v$APP_VERSION/zecpaperwallet
+cd artifacts
+cd aarch64-zecpaperwallet-v$APP_VERSION
+gsha256sum zecpaperwallet > sha256sum.txt
+cd ..
+zip -r aarch64-zecpaperwallet-v$APP_VERSION.zip aarch64-zecpaperwallet-v$APP_VERSION 
+cd ..
 
